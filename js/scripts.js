8d3db7a5e65f536bc566aa7b1c0250b8b7993e37
oliveroliverio <oliveroliverio@gmail.com>
2024-08-13 15:03:31 -0700

    added pokemon to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,30 @@
-alert('hello world')
+let pokemonList = []
 
-let favoriteFood = 'pizza'
-document.write(favoriteFood)
+pokemonList.push({
+	name: 'Bulbasaur',
+	height: 7,
+	types: ['grass', 'poison'],
+})
+
+let Charmeleon = {
+	name: 'Charmeleon',
+	height: 3,
+	types: ['fire', 'orange'],
+}
+let Pikachu = {
+	name: 'Pikachu',
+	height: 5,
+	types: ['lightning', 'yellow'],
+}
+let MewToo = {
+	name: 'MewToo',
+	height: 3,
+	types: ['telekinesis', 'purple'],
+}
+let Dinosauuuurr = {
+	name: 'Dinosauuur',
+	height: 8,
+	types: ['veryStrong', 'green'],
+}
+
+pokemonList.push(Charmeleon, Pikachu, MewToo, Dinosauuuurr)
