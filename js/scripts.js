554cc0aade2bf7e12f95154721786f3d114a7869
oliveroliverio <oliveroliverio@gmail.com>
2024-08-15 13:53:46 -0700

    refactor for loop to forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -29,20 +29,32 @@ let Dinosauuuurr = {
 
 pokemonList.push(Charmeleon, Pikachu, MewToo, Dinosauuuurr)
 
-for (let i = 0; i < pokemonList.length; i++) {
-	// console.log(pokemonList[i])
-	// console.log(pokemonList[i].height)
-	if (pokemonList[i].height >= 7) {
-		document.write(
-			JSON.stringify(pokemonList[i].name) + "  You're big!!!!" + '<br><br>'
-		)
-	}
-	if (pokemonList[i].types[0] === 'telekinesis') {
-		document.write(
-			JSON.stringify(pokemonList[i].name) +
-				"  I'm a huge fan of Eleven from Stranger Things!!!!!" +
-				'<br><br>'
-		)
-	}
-	// document.body.innerHTML += JSON.stringify(pokemonList[i]) + '<br>'
-}
+// for (let i = 0; i < pokemonList.length; i++) {
+// 	// console.log(pokemonList[i])
+// 	// console.log(pokemonList[i].height)
+// 	if (pokemonList[i].height >= 7) {
+// 		document.write(
+// 			JSON.stringify(pokemonList[i].name) + "  You're big!!!!" + '<br><br>'
+// 		)
+// 	}
+// 	if (pokemonList[i].types[0] === 'telekinesis') {
+// 		document.write(
+// 			JSON.stringify(pokemonList[i].name) +
+// 				"  I'm a huge fan of Eleven from Stranger Things!!!!!" +
+// 				'<br><br>'
+// 		)
+// 	}
+// 	// document.body.innerHTML += JSON.stringify(pokemonList[i]) + '<br>'
+// }
+
+// refactor the above to forEach loop
+pokemonList.forEach(function (pokemon) {
+	document.write(
+		JSON.stringify(pokemon.name) +
+			'<br>' +
+			JSON.stringify(pokemon.height) +
+			'<br>' +
+			JSON.stringify(pokemon.types) +
+			'<br><br>'
+	)
+})
