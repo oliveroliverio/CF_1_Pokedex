d39aeda6a3a1ab5bcd56ea0479b02c872a6fbe5c
oliveroliverio <oliveroliverio@gmail.com>
2024-08-27 14:21:29 -0700

    added validation to form

--- js/scripts.js
+++ js/scripts.js
@@ -140,12 +140,65 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function validateEmail() {
 	let value = emailInput.value
-	let hasAtSign = value.indexOf('@') > -1
-	let hasDot = value.indexOf('.') > -1
-	return value && hasAtSign && hasDot
-}
 
+	if (!value) {
+		showErrorMessage(emailInput, 'Email is a required field.')
+		return false
+	}
+
+	if (value.indexOf('@') === -1) {
+		showErrorMessage(emailInput, 'You must enter a valid email address.')
+		return false
+	}
+
+	if (value.indexOf('.') === -1) {
+		showErrorMessage(emailInput, 'You must enter a valid email address.')
+		return false
+	}
+
+	showErrorMessage(emailInput, null)
+	return true
+}
 function validatePassword() {
 	let value = passwordInput.value
-	return value && value.length >= 8
+
+	if (!value) {
+		showErrorMessage(passwordInput, 'Password is a required field.')
+		return false
+	}
+
+	if (value.length < 8) {
+		showErrorMessage(
+			passwordInput,
+			'The password needs to be at least 8 characters long.'
+		)
+		return false
+	}
+
+	showErrorMessage(passwordInput, null)
+	return true
+}
+
+function showErrorMessage(input, message) {
+	let container = input.parentElement // The .input-wrapper
+
+	// Check and Remove any existing errors
+	let error = container.querySelector('.error-message')
+	if (error) {
+		container.removeChild(error)
+	}
+
+	// Now add the error if the message isn’t empty
+	if (message) {
+		let error = document.createElement('div')
+		error.classList.add('error-message')
+		error.innerText = message
+		container.appendChild(error)
+	}
+}
+
+function validateForm() {
+	let isValidEmail = validateEmail()
+	let isValidPassword = validatePassword()
+	return isValidEmail && isValidPassword
 }
